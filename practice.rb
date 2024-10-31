08a7cd0d7aab54d9ea6a1f3f75d6b882457e3132
Iconia7 <mwanginewton239@gmail.com>
2024-10-31 09:56:57 +0300

    Fix:ask user to enter

--- practice.rb
+++ practice.rb
@@ -1,36 +1,42 @@
 class Person
-  @@population_count = 0         # Class variable
+  @@count = 0         
 
   def initialize(name, age)
-    @name = name                 # Instance variable
+    @name = name                 
     @age = age
-    @@population_count += 1
+    @@count += 1
   end
 
-  def introduce
-    greeting = "Hello"           # Local variable
+  def introduction
+    greeting = "Hello"          
     puts "#{greeting}, I'm #{@name} and I'm #{@age} years old."
   end
 
   def self.population
-    puts "There are #{@@population_count} people."
+    puts "There are #{@@count} people."
   end
 end
 
-# Global variable example
-$global_var = "I'm accessible anywhere in this Ruby program!"
+$global = "Global variable"
 
-# Constant
 PI = 3.14159
 
-# Creating instances of Person
-person1 = Person.new("Alice", 30)
-person2 = Person.new("Bob", 25)
+puts "Enter the name for the first person:"
+name1 = gets.chomp
+puts "Enter the age for the first person:"
+age1 = gets.chomp.to_i
 
-# Calling methods
-person1.introduce               # => "Hello, I'm Alice and I'm 30 years old."
-person2.introduce               # => "Hello, I'm Bob and I'm 25 years old."
-Person.population               # => "There are 2 people."
+puts "Enter the name for the second person:"
+name2 = gets.chomp
+puts "Enter the age for the second person:"
+age2 = gets.chomp.to_i
 
-puts PI                         # => 3.14159
-puts $global_var                # => "I'm accessible anywhere in this Ruby program!"
+person1 = Person.new(name1, age1)
+person2 = Person.new(name2,age2)
+
+person1.introduction             
+person2.introduction             
+Person.population               
+
+puts PI                       
+puts $global               
